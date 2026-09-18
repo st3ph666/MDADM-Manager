483f13e77ae6f86311275d18782e9b69c307790a
SexoScript <154566407+st3ph666@users.noreply.github.com>
2026-09-17 21:41:46 -0400

    Update modular architecture for v1.76

--- mdadm_matrix/__init__.py
+++ mdadm_matrix/__init__.py
@@ -1,4 +1,4 @@
-"""Paquet principal de MDADM Manager."""
+"""Main package for MDADM Manager."""
 
-APP_VERSION = "1.75"
+APP_VERSION = "1.76"
 APP_TITLE = f"MDADM Manager v{APP_VERSION} // MATRIX ROOT"
